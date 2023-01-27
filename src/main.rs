255669c99d3d812dae594b84fd985f4cf95aa5d4
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-01-27 12:15:04 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn q_rsqrt(number: f32) -> f32 {
+    let mut i: i32;
+    let x2: f32;
+    let mut y: f32;
+    const THREEHALFS: f32 = 1.5;
+    x2 = number * 0.5;
+    y = number;
+    i = unsafe { std::mem::transmute::<f32, i32>(y) };
+    i = 0x5f3759df - (i >> 1);
+    y = unsafe { std::mem::transmute::<i32, f32>(i) };
+    y = y * (THREEHALFS - (x2 * y * y));
+    return y;
+}
+
+fn main(){
+    let ans = q_rsqrt(46.6);
+    println!("{}", ans);
+}
